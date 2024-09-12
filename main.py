0ab20ff67c74b7f0290bb7a7adabbb91df20bb94
Ryan Adrian <125546701+NayrAdrian@users.noreply.github.com>
2024-09-12 18:17:45 +0800

    Integrated smtp email sending feature for price alerts when the current price drops below the target. Added support for securely loading email credentials from environment variables using python-dotenv.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+import smtplib
 from bs4 import BeautifulSoup
 import requests
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Email credentials and target recipient
+EMAIL_ADDRESS = os.environ["MY_EMAIL"]
+EMAIL_PASSWORD = os.environ["MY_EMAIL_PASSWORD"]
+TARGET_EMAIL = os.environ["MY_TARGET_EMAIL"]
+
+# Target price
+TARGET_PRICE = 100.00  # Set your target price
 
 # Fetch the web page
 response = requests.get("https://appbrewery.github.io/instant_pot/")
@@ -11,6 +25,32 @@ offscreen_price = soup.find('span', class_='aok-offscreen')
 # Get the price if the element is found and remove the '$' sign
 if offscreen_price:
     price = offscreen_price.getText().strip().replace('$', '')
-    print(f"Price: {price}")
+    current_price = float(price)
+    print(f"Current Price: {current_price}")
+
+    # Check if the current price is below the target price
+    if current_price < TARGET_PRICE:
+        print("Sending email, current price is below the target.")
+
+        # Setup SMTP Connection
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls() # Secures the connection
+            connection.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+            # Create email content
+            subject = "Price Alert: Instant Pot Below Target Price!"
+            body = f"The current price is ${current_price}, whice is below your target price ${TARGET_PRICE}."
+            msg = f"Subject: {subject}\n\n{body}"
+
+            # Send email
+            connection.sendmail(
+                from_addr=EMAIL_ADDRESS,
+                to_addrs=TARGET_EMAIL,
+                msg=msg
+            )
+        print("Email sent successfully.")
+    else:
+        print("Current price is above the target, no email sent")
 else:
     print("Price not found")
+
