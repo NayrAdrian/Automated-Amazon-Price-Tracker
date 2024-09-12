820e6ea814614da95357482d38de7034a7aac9ce
Ryan Adrian <125546701+NayrAdrian@users.noreply.github.com>
2024-09-12 18:30:13 +0800

    Added custom headers to web scraping script for simulating browser behavior. Updated request headers to match actual HTTP request headers from the browser for improved compatibility and reliability.

--- main.py
+++ main.py
@@ -12,11 +12,26 @@ EMAIL_ADDRESS = os.environ["MY_EMAIL"]
 EMAIL_PASSWORD = os.environ["MY_EMAIL_PASSWORD"]
 TARGET_EMAIL = os.environ["MY_TARGET_EMAIL"]
 
+# Custom headers
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
+              "application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "cross-site",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 "
+                  "Safari/537.36",
+}
+
 # Target price
 TARGET_PRICE = 100.00  # Set your target price
 
 # Fetch the web page
-response = requests.get("https://appbrewery.github.io/instant_pot/")
+response = requests.get("https://appbrewery.github.io/instant_pot/", headers=header)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # Locate the offscreen price element
